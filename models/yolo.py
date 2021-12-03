corrected nan gradients by removing sqrt and after all tests

models/yolo.py
@@ -70,8 +70,10 @@ class Detect(nn.Module):
                 # y = x[i].sigmoid()
                 aa=x[i][:, :, :, :, :4].sigmoid()
                 # bb = torch.arcsin(x[i][:, :, :, :, 4:5].tanh())
+                # bb = torch.arcsin(x[i][:, :, :, :, 4:5])
                 # bb = torch.arcsin(x[i][:, :, :, :, 4:5].tanh())*0
-                bb = torch.arcsin(x[i][:, :, :, :, 4:5].sigmoid()*2-1)*0
+                # bb = torch.arcsin(x[i][:, :, :, :, 4:5].sigmoid() * 2 - 1.0)
+                bb = torch.arcsin(x[i][:, :, :, :, 4:5].tanh())
                 cc = x[i][:, :, :, :, 5:].sigmoid()
                 # # print("aa ", aa.shape)
                 # # print("bb ", bb.shape)

utils/datasets.py
@@ -431,7 +431,10 @@ class LoadImagesAndLabels(Dataset):
         try:
             with open(json_dataset, "r") as fin:
                 json_data = json.load(fin)
-            self.img_files = [data["file_name"] for data in json_data][:1000] ###################### LIMIT SIZE
+            if "test" in json_dataset:
+                self.img_files = [data["file_name"] for data in json_data][:300] ###################### LIMIT SIZE
+            else:
+                self.img_files = [data["file_name"] for data in json_data]#[:1500] ###################### LIMIT SIZE
             self.annotations = []
             for fi, f in enumerate(self.img_files):
                 data = json_data[fi]
@@ -443,15 +446,15 @@ class LoadImagesAndLabels(Dataset):
                     cat = det["object_id"]
                     # ell = det["ellipse"]
 
-                    ## bbox 
-                    x1, y1, x2, y2 = det["bbox"]
-                    center = [(x1 + x2) / 2, (y1 + y2) / 2]
-                    axes = [(x2 - x1), (y2 - y1)] # for now the network wants to receive xywh
-                    center[0] /= w
-                    center[1] /= h
-                    axes[0] /= w
-                    axes[1] /= h
-                    label = [cat] + center + axes + [0.0] #[sin_angle]
+                    # ## bbox 
+                    # x1, y1, x2, y2 = det["bbox"]
+                    # center = [(x1 + x2) / 2, (y1 + y2) / 2]
+                    # axes = [(x2 - x1), (y2 - y1)] # for now the network wants to receive xywh
+                    # center[0] /= w
+                    # center[1] /= h
+                    # axes[0] /= w
+                    # axes[1] /= h
+                    # label = [cat] + center + axes + [0.0] #[sin_angle]
 
                     # if np.any(np.array(label[:-1]) < 0):
                     #     print("="*100)
@@ -459,14 +462,14 @@ class LoadImagesAndLabels(Dataset):
                     #     print("="*100)
 
                     # ellipses
-                    # ell = det["ellipse"]
-                    # axes = np.asarray(ell["axes"]) * 2
-                    # center = np.asarray(ell["center"])
-                    # center[0] /= w
-                    # center[1] /= h
-                    # axes[0] /= w
-                    # axes[1] /= h
-                    # label = [cat] + center.tolist() + axes.tolist() + [ell["angle"]]
+                    ell = det["ellipse"]
+                    axes = np.asarray(ell["axes"]) * 2
+                    center = np.asarray(ell["center"])
+                    center[0] /= w
+                    center[1] /= h
+                    axes[0] /= w
+                    axes[1] /= h
+                    label = [cat] + center.tolist() + axes.tolist() + [ell["angle"]]
 
                     annots.append(label)
                 self.annotations.append(np.array(annots))

utils/loss.py
@@ -9,6 +9,7 @@ import torch.nn as nn
 from utils.metrics import bbox_iou, ellipses_sampling_distance
 from utils.torch_utils import is_parallel
 from math import pi
+import numpy as np
 
 
 def smooth_BCE(eps=0.1):  # https://github.com/ultralytics/yolov3/issues/238#issuecomment-598028441
@@ -114,8 +115,9 @@ class ComputeLoss:
         self.BCEcls, self.BCEobj, self.gr, self.hyp, self.autobalance = BCEcls, BCEobj, 1.0, h, autobalance
         for k in 'na', 'nc', 'nl', 'anchors':
             setattr(self, k, getattr(det, k))
+        self.DEBUG = []
 
-    def __call__(self, p, targets):  # predictions, targets, model
+    def __call__(self, p, targets, epoch=-1):  # predictions, targets, model
         # print("NC = ", self.nc)
         # print("predictions size = ", len(p))
         # print(p[0].shape, p[1].shape, p[2].shape)
@@ -129,7 +131,8 @@ class ComputeLoss:
         # print(tangle[0].shape)
         # print(len(indices), len(indices[0]), indices[0][0].shape)
         # print(anchors[0].shape)
-
+        # if epoch != -1:
+        #     print("===========================>", epoch)
         # Losses
         for i, pi in enumerate(p):  # layer index, layer predictions
             b, a, gj, gi = indices[i]  # image, anchor, gridy, gridx
@@ -148,6 +151,7 @@ class ComputeLoss:
                 pxy = ps[:, :2].sigmoid() * 2. - 0.5
                 pwh = (ps[:, 2:4].sigmoid() * 2) ** 2 * anchors[i]
 
+                # pa = ps[:, 4:5].sigmoid() * 2 - 1.0#.tanh()
                 pa = ps[:, 4:5].tanh()
                 pbox = torch.cat((pxy, pwh), 1)  # predicted box
                 # print("pbox = ", pbox.shape)
@@ -156,7 +160,9 @@ class ComputeLoss:
                 # print(pa[:10, :])
                 # print(pbox.grad)
 
-                # l_ell = ellipses_sampling_distance(torch.cat((pbox, pa), 1), torch.cat((tbox[i], torch.sin(tangle[i])), 1))
+                l_ell = ellipses_sampling_distance(torch.cat((pbox, pa), 1), torch.cat((tbox[i], torch.sin(tangle[i])), 1))
+
+                # lbox += torch.mean(torch.sum((pbox - tbox[i])**2, dim=1))
                 
                 # l_ell = ellipses_sampling_distance(pbox, tbox[i])
 
@@ -171,20 +177,32 @@ class ComputeLoss:
                 # lbox += aa + bb * 4
                 # lbox += torch.mean((pbox - tbox[i])**2) + torch.mean((ps[:, 4].tanh())**2)
 
-                lbox += (1.0 - iou).mean()  # iou loss
-                # lbox += torch.mean(torch.abs(torch.sin(tangle[i]) - pa))
-                # lbox += l_ell
+                # lbox += (1.0 - iou).mean()  # iou loss
+                # lbox += torch.mean(torch.abs((pbox-tbox[i])))
+                # pbox.register_hook(lambda grad: print(grad))
+                # pa.register_hook(lambda grad: print(grad))
+
+                # if epoch >= 10:
+                #     lbox += l_ell*0.1 #0.0001
+
+                # lbox += torch.mean((torch.sin(tangle[i]) - pa)**2)
+                # lbox += l_ell * 0.0001
+                # lbox += l_ell * 0.1
+                # lbox += l_ell * 0.025  # 0.025
+                
+                # lbox += l_ell * 0.0125  # 0.025
+                lbox += l_ell
 
                 # Objectness
                 score_iou = iou.detach().clamp(0).type(tobj.dtype)
                 if self.sort_obj_iou:
                     sort_id = torch.argsort(score_iou)
                     b, a, gj, gi, score_iou = b[sort_id], a[sort_id], gj[sort_id], gi[sort_id], score_iou[sort_id]
-                tobj[b, a, gj, gi] = (1.0 - self.gr) + self.gr * score_iou  # iou ratio
+                tobj[b, a, gj, gi] = (1.0 - self.gr) + self.gr * score_iou  # iou ratio ([mz] used as gt label for conf)
 
                 # Classification
                 if self.nc > 1:  # cls loss (only if multiple classes)
-                    t = torch.full_like(ps[:, 6:], self.cn, device=device)  # targets
+                    t = torch.full_like(ps[:, 6:], self.cn, device=device)  # targets ([mz] logits with self.cn/self.cp for negative/positive)
                     t[range(n), tcls[i]] = self.cp
                     lcls += self.BCEcls(ps[:, 6:], t)  # BCE
 
@@ -192,7 +210,7 @@ class ComputeLoss:
                 # with open('targets.txt', 'a') as file:
                 #     [file.write('%11.5g ' * 4 % tuple(x) + '\n') for x in torch.cat((txy[i], twh[i]), 1)]
 
-            obji = self.BCEobj(pi[..., 5], tobj)
+            obji = self.BCEobj(pi[..., 5], tobj) # loss for confidence between the predicted conf pi and the iou ratio tobj
             lobj += obji * self.balance[i]  # obj loss
             if self.autobalance:
                 self.balance[i] = self.balance[i] * 0.9999 + 0.0001 / obji.detach().item()
@@ -205,6 +223,9 @@ class ComputeLoss:
         lbox *= self.hyp['box']
         lobj *= self.hyp['obj']
         lcls *= self.hyp['cls']
+        # print(lobj)
+        # self.DEBUG.append(lobj.cpu().detach().numpy())
+
         bs = tobj.shape[0]  # batch size
 
         return (lbox + lobj + lcls) * bs, torch.cat((lbox, lobj, lcls)).detach()
@@ -245,8 +266,14 @@ class ComputeLoss:
             if nt:
 
                 # [mz] First filtering
+                # print("t = ", t[:, :, 4:6])
+                # print("anchors = ", anchors[:, None])
+                # print("t = ", t[:, :, 4:6].shape)
+                # print("anchors = ", anchors[:, None].shape)
+
                 # Matches
                 r = t[:, :, 4:6] / anchors[:, None]  # wh ratio
+                # print("r = ", r.shape)
                 # print("anchors[:, None] = ", anchors[:, None])
                 # print(t.shape)
                 # print(anchors.shape)
@@ -283,10 +310,10 @@ class ComputeLoss:
             gi, gj = gij.T  # grid xy indices
 
             # filter bad angles
-            # sup = torch.where(t[:, 6] > pi/2)[0]
-            # inf = torch.where(t[:, 6] < -pi/2)[0]
-            # t[sup, 6] -= pi
-            # t[inf, 6] += pi
+            sup = torch.where(t[:, 6] > pi/2)[0]
+            inf = torch.where(t[:, 6] < -pi/2)[0]
+            t[sup, 6] -= pi
+            t[inf, 6] += pi
 
 
             # Append
@@ -310,3 +337,6 @@ class ComputeLoss:
             # print("anch = ", anch)
 
         return tcls, tbox, tangle, indices, anch
+
+    def save_DEBUG(self):
+        np.savetxt("/home/mzins/dev/yolov5/runs/debug.txt", self.DEBUG)

utils/metrics.py
@@ -10,6 +10,7 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+import torch.nn as nn
 
 
 def fitness(x):
@@ -351,7 +352,8 @@ def generate_sampling_points(range_x, range_y, sampling_x, sampling_y):
 
 def sample_ellipse(axes, sin, center, points):
     A = torch.diag(axes)
-    cos = torch.sqrt(1 - sin**2)
+    epsilon = 1.e-3
+    cos = torch.sqrt(1 - sin**2 + epsilon)
     R = torch.cat((torch.cat((cos, -sin), dim=1),
                     torch.cat((sin, cos), dim=1)), dim=0)
     
@@ -376,26 +378,58 @@ range_x = [-2.0, 2.0]
 range_y = [-2.0, 2.0]
 sampling_x = 20
 sampling_y = 20
+N = sampling_x * sampling_y
 points = generate_sampling_points(range_x, range_y, sampling_x, sampling_y)
 
 IIDX = 0
 
+# def ellipses_sampling_distance(boxes1, boxes2):
+#     # print(boxes1.shape, boxes2.shape)
+#     global points, IIDX, N
+#     vals = []
+#     # print(boxes1[:5, :])
+#     for bb1, bb2 in zip(boxes1, boxes2):
+#         # s1 = sample_ellipse(bb1[2:4]/2, torch.zeros([1, 1], device=torch.device('cuda:0')), bb1[:2], points)
+#         # s2 = sample_ellipse(bb2[2:4]/2, torch.zeros([1, 1], device=torch.device('cuda:0')), bb2[:2], points)
+#         s1 = sample_ellipse(bb1[2:4]/2, bb1[4].reshape((1, 1)), bb1[:2], points)
+#         s2 = sample_ellipse(bb2[2:4]/2, bb2[4].reshape((1, 1)), bb2[:2], points)
+#         # vals.append(torch.sqrt((torch.sum((s1 - s2)**2) / (sampling_x * sampling_y))).unsqueeze(0))
+#         # vals.append((torch.sum(torch.abs(s1 - s2)) / N).unsqueeze(0))
+#         vals.append((torch.sum((s1 - s2)**2) / N).unsqueeze(0))
+#         if IIDX % 50000 == 0:
+#             save_sampling(points.T, s1, "s1")
+#             save_sampling(points.T, s2, "s2")
+#         IIDX += 1
+
+#     l =  torch.mean(torch.cat(vals))
+#     return l
+
+
+
 def ellipses_sampling_distance(boxes1, boxes2):
     # print(boxes1.shape, boxes2.shape)
-    global points, IIDX
+    global points, IIDX, N
     vals = []
+    # S1, S2 = [], []
     # print(boxes1[:5, :])
     for bb1, bb2 in zip(boxes1, boxes2):
         # s1 = sample_ellipse(bb1[2:4]/2, torch.zeros([1, 1], device=torch.device('cuda:0')), bb1[:2], points)
         # s2 = sample_ellipse(bb2[2:4]/2, torch.zeros([1, 1], device=torch.device('cuda:0')), bb2[:2], points)
         s1 = sample_ellipse(bb1[2:4]/2, bb1[4].reshape((1, 1)), bb1[:2], points)
         s2 = sample_ellipse(bb2[2:4]/2, bb2[4].reshape((1, 1)), bb2[:2], points)
-        vals.append(torch.sqrt((torch.sum((s1 - s2)**2) / (sampling_x * sampling_y))).unsqueeze(0))
+        # vals.append(torch.sqrt((torch.sum((s1 - s2)**2) / (sampling_x * sampling_y))).unsqueeze(0))
+        # vals.append((torch.sum(torch.abs(s1 - s2)) / N).unsqueeze(0))
+
+        vals.append((s1 - s2)**2)
+        # S1.append(s1)
+        # S2.append(s2)
+
+        # vals.append(torch.abs(s1 - s2))
         if IIDX % 50000 == 0:
             save_sampling(points.T, s1, "s1")
             save_sampling(points.T, s2, "s2")
         IIDX += 1
-
     l =  torch.mean(torch.cat(vals))
+    # l = nn.SmoothL1Loss(reduction="mean")(torch.cat(S1), torch.cat(S2))
     return l
 